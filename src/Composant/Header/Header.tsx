72e5d942e8c21c64503cd1f943b8d9afaebd1ef5
MaximeG44270 <gilbert.maxime01@gmail.com>
2025-03-19 16:49:23 +0100

    correction image logo

--- src/Composant/Header/Header.tsx
+++ src/Composant/Header/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
     <header id="accueil" className="w-full bg-color text-white pr-6 md:pr-8 lg:pr-14 box-border top-0 left-0">
       <div className="mt-14 flex justify-between items-center">
         {/* <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold font-poppins">MG Digital</h1> */}
-        <img src="/public/logo-transparent.webp" alt="Image 1" className="w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 2xl:w-56" />
+        <img src="/logo-transparent.webp" alt="Image 1" className="w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 2xl:w-56" />
         <NavBar />
       </div>
     </header>
